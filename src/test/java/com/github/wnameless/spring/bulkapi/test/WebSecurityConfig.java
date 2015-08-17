b342fa5856998021ff1ecd0b7492d2a4d53968d9
wmw <wnameless@gmail.com>
2015-08-17 21:00:48 +0800

    Correct WebSecurityConfig for testing

--- src/test/java/com/github/wnameless/spring/bulkapi/test/WebSecurityConfig.java
+++ src/test/java/com/github/wnameless/spring/bulkapi/test/WebSecurityConfig.java
@@ -38,6 +38,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable();
+    http.authorizeRequests().antMatchers("/home").hasRole("USER").and()
+        .httpBasic();
     http.authorizeRequests().antMatchers(bulkPath).permitAll();
   }
 
